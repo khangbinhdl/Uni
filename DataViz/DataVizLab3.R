0f0e34abe1842fa9e35cd61b072718246fa28657
Phạm Khang Bình <82141543+khangbinhdl@users.noreply.github.com>
2024-04-08 15:55:51 +0700

    Create DataVizLab3.R

---
+++ DataViz/DataVizLab3.R
@@ -0,0 +1,43 @@
+# Bai 1
+data01 <- read.csv('D:/DH/Học kì 2/TQHDL/TuyenSinh2022.csv', header = T)
+data01
+myCol <- c("lavender", "lightblue", "mistyrose",
+           "cornsilk","orange3",
+           "lightcyan", " darkolivegreen3", "yellow2","navy","red4")
+KhoiNganh = data01$Khoi_Nganh
+names(KhoiNganh)<-paste("Khoi nganh", KhoiNganh, ":", data01$Ty_le, sep=" ")
+pie(KhoiNganh, clockwise = T, radius = 1,
+    border = "black", col = myCol, init.angle = 90, cex = 0.8)
+
+# Bai 2
+data02 <- read.csv('D:/DH/Học kì 2/TQHDL/SmartPhoneMarket2022-2023.csv', header = T)
+data02
+par(mfrow=c(1,3))
+pie(data02$Proportion_2022, data02$Proportion_2022, radius = 1, col = myCol,
+    border = "darkgreen", init.angle = 90, cex = 0.8)
+mtext("                      2022", adj = 0, line = -25, outer = T)
+pie(data02$Proportion_2023, data02$Proportion_2023, radius = 1, col = myCol,
+    border = "darkgreen", init.angle = 90, cex = 0.8)
+mtext("                                                                      2023", adj = 0, line = -25, outer = T)
+plot.new()
+legend("right", data02$Companies, fill = myCol)
+mtext("Unit: %", side = 1, line=-3.5, adj =0, cex=1.5, outer=T)
+
+# Bai 3
+
+pie_myData<- c(5.8,27.0,0.2,21.1,12.8,33.1)
+energytypes <- c( "Nuclear energy", "Coal", "Others", "Gas", "Renewable energies", "Oil")
+par(mfrow=c(1,1),xpd=TRUE)
+myBar <- barplot(pie_myData, names.arg = energytypes, col = myCol, border="darkgreen",
+        ylab = "Tỷ trọng (%)", ylim = c(0, 40), cex.names = 0.6, 
+        main = "Energy sources trong nguồn cung cấp năng lượng chính năm 2008")
+
+text(x=myBar , y=pie_myData , label=pie_myData , pos=3, cex=0.65, col="red2")
+
+# Bai 4
+par(mfrow=c(1,1),xpd=TRUE)
+smartData = rbind(data02$Proportion_2022, data02$Proportion_2023)
+smartChart <- barplot(smartData, names.arg = data02$Companies, beside = T, col = c("lavender","cornsilk"), 
+              ylab = "Ty trong (%)", ylim = c(0, 80), border = "darkgreen",
+              main = "Ty trong smartphone cua cac hang trong 2022 va 2023")
+text(x=smartChart , y=smartData , label=smartData , pos=3, cex=0.65, col=c("red2", "blue"))
